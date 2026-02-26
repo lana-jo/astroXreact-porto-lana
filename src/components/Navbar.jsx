b18ebe3983d1d1925209306c4d17012b387ece39
jo <lanajauhar06@gmail.com>
2026-02-27 06:47:32 +0700

    init commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+    const [lastScroll, setLastScroll] = useState(0);
+    const [hidden, setHidden] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.pageYOffset;
+
+            if (currentScroll > lastScroll) {
+                setHidden(true);
+            } else {
+                setHidden(false);
+            }
+
+            setLastScroll(currentScroll);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [lastScroll]);
+
+    return (
+        <nav
+            className="menu card"
+            style={{
+                transform: hidden ? "translateY(-100%)" : "translateY(0)",
+                transition: "transform 0.3s ease",
+            }}
+        >
+            <a href="#home">Home</a>
+            <a href="#aboutme">About Me</a>
+            <a href="#techstack">Tech Stack</a>
+            <a href="#porto">Porto</a>
+            <a href="#certif">Certificate</a>
+        </nav>
+    );
+}
