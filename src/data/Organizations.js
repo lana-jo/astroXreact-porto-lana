83a548a5f0b39fa0d633d24c4a34fde5ed5d25f9
jo <lanajauhar06@gmail.com>
2026-03-09 17:44:07 +0700

    by date desc

--- src/data/Organizations.js
+++ src/data/Organizations.js
@@ -1,5 +1,14 @@
 export const organizations = [
     {
+        period: "July 2020 - November 2021",
+        role: "Hubungan Masyarakat",
+        company: "Himpunan Mahasiswa Rekayasa Perangkat Lunak",
+        descriptions: [
+            "providing a space for discussion about software engineering.",
+            "talk show for student personal development.",
+        ]
+    },
+    {
         period: "July 2019 - July 2021",
         role: "Hubungan Masyarakat",
         company: "Badan Eksekutif Mahasiswa",
@@ -7,15 +16,6 @@ export const organizations = [
             "cleaning up the campus surroundings and the surrounding environment.",
             "talk show for student personal development .",
             "Responsible for the running of the “ngaji bareng” event, which is a Quran recitation and religious lecture event. ",
-        ]
-    },
-    {
-        period: "July 2020 - November 2021",
-        role: "Hubungan Masyarakat",
-        company: "Himpunan Mahasiswa Rekayasa Perangkat Lunak",
-        descriptions: [
-            "providing a space for discussion about software engineering.",
-            "talk show for student personal development.",
         ]
     },
 ]
